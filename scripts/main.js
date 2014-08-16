46ff20f330df8399dc202797ed84762ad3c2fe4f
Srijan R Shetty <srijan.shetty@gmail.com>
2014-08-16 18:52:45 +0530

    Whitespace in register

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
 var app = angular.module("root", [])
-				.controller('index', ['$scope', function($scope) {
+				.controller('index', ['$scope', '$http', function($scope, $http) {
 					$scope.submit = function() {
 						console.log($scope.user);
+						$http({
+							method: 'POST',
+							url: 'register.php',
+							data: $scope.user
+						})
+						.success(function() {
+							console.log('success');
+						})
+						.error(function() {
+							console.log('fail');
+						});
 					}
 				}]);
