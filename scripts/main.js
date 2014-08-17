c0060256f17b2256ce4fb351e9ef51fac87ddaa5
Srijan R Shetty <srijan.shetty@gmail.com>
2014-08-17 21:23:55 +0530

    Production version

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,1 @@
-var app = angular.module("root", [])
-				.controller('index', ['$scope', '$http', function($scope, $http) {
-					$scope.submit = function() {
-						console.log($scope.user);
-						$http({
-							method: 'POST',
-							url: 'register.php',
-							data: $scope.user
-						})
-						.success(function() {
-							console.log('success');
-						})
-						.error(function() {
-							console.log('fail');
-						});
-					}
-				}]);
+var app = angular.module('root', []);
